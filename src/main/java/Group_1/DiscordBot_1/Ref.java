133997d7e8974554320881c8f90c1fc3ddb3cc66
Robert Noonan <39569282+robNoonan@users.noreply.github.com>
2019-08-19 10:11:56 -0400

    Update Ref.java

--- src/main/java/Group_1/DiscordBot_1/Ref.java
+++ src/main/java/Group_1/DiscordBot_1/Ref.java
@@ -1,6 +1,6 @@
 package Group_1.DiscordBot_1;
 
 public class Ref {
-	public static final String token = "NDU4NzI0NDQwNjgzODM5NDkw.DgsuaQ.ZZlnTLucsc95ThvgjiRKbnjX1K0";
+	public static final String token = "";
 	public static final String prefix = ">";
 }
